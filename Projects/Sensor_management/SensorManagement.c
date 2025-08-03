eae2bb70e2c0e23511b10ec5b4a83b2d0e741933
Thamaraimanalan M <mahasel.1969@gmail.com>
2025-08-03 11:49:01 +0530

    Update Project 3

--- Projects/Sensor_management/SensorManagement.c
+++ Projects/Sensor_management/SensorManagement.c
@@ -68,14 +68,14 @@ void logger(char *message,  logging log_level); // from logger.c
 // Main entry point
 int main(){
 
-    Sensor sensor_list[2] = {0};
+    Sensor sensor_list[10] = {0};
     uint8_t sensor_count = 0;
     uint8_t init_sensor = 0;
     uint8_t stype;
     
     while (1){
         
-        if (sensor_count >= 2) 
+        if (sensor_count >= 10) 
         {
             logger("Sensor count >= 10", LOG_ERROR);
             break;
@@ -134,7 +134,7 @@ int main(){
             
             case HUMIDITY:
 
-                logger("Enter Caliberation of Temperature: ", LOG_INPUT);
+                logger("Enter Caliberation of Humidity: ", LOG_INPUT);
                 scanf("%f", &sensor_list[sensor_count].data.Humidity.caliberation);
                 scanf("*c");
 
@@ -160,7 +160,8 @@ int main(){
                 break;
         }
 
-        
+        sensor_list[sensor_count].status = ACTIVE;
+
         logger("Press 1 to add sensor or 0 to skip: ", LOG_INPUT);
         scanf("%hhu", &init_sensor); // hhu - unsigned char
         scanf("*c"); // consume a leftover newline character (\n) that remains in the input buffer after a previous scanf call
@@ -170,19 +171,48 @@ int main(){
             break;
         }
 
-        sensor_list[sensor_count].status = ACTIVE;
         sensor_count = sensor_count + 1;
     }
 
     FILE *fptr;
     fptr = fopen("Sensor_out.txt", "w");
 
-    for ( int i = 0; i < sensor_count; i++){
+    if (fptr == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    for ( int i = 0; i <= sensor_count; i = i + 1){
 
         fprintf(fptr, "[DEBUG] ID: %d \n", sensor_list[i].id);
         fprintf(fptr, "[DEBUG] Name: %s \n", sensor_list[i].name);
         fprintf(fptr, "[DEBUG] Type: %u \n", sensor_list[i].type);
-        
+
+        switch (sensor_list[i].type)
+        {
+            case TEMPERATURE:
+
+                fprintf(fptr, "Enter Minimum range of Temperature: %hd \n", sensor_list[i].data.Temperature.min_range);
+                fprintf(fptr, "Enter Maximum range of Temperature: %hd \n", sensor_list[i].data.Temperature.max_range);
+                fprintf(fptr, "Enter Readings range of Temperature: %f \n", sensor_list[i].data.Temperature.reading);
+                
+            break;
+            
+            case HUMIDITY:
+
+                fprintf(fptr, "Enter Caliberation of Temperature: %f \n", sensor_list[i].data.Humidity.caliberation);
+                fprintf(fptr, "Enter Readings range of Humiditiy: %f \n", sensor_list[i].data.Humidity.reading);
+          
+            break;
+
+            case PRESSURE:
+
+                fprintf(fptr, "Enter Caliberation of Pressure: %hd \n", sensor_list[i].data.Pressure.altitude);
+                fprintf(fptr, "Enter Readings range of Pressure: %f \n", sensor_list[i].data.Pressure.reading);
+                
+            break;
+             
+        }
     }
 
     fclose(fptr);
